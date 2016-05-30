6a81d8a9a579cab022f62fe0e8b2b9bb8b696244
Masroor_Shah <masroorsherazi12@gmail.com>
2016-05-30 15:41:07 +0500

    build auto

---
+++ BuildAutomation/src/buildAutomation/Code.java
@@ -0,0 +1,13 @@
+package buildAutomation;
+
+public class Code {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Build.............\n..............Automation"
+				+ "\n....using\n................Ant");
+
+	}
+
+}
